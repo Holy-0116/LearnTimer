8629b8c37fc5af0f89ae67db2abb7044dd803773
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-12-21 16:16:43 +0900

    first_commit

---
+++ LearnTimer/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  LearnTimer
+//
+//  Created by 堀口達也 on 2020/12/21.
+//
+
+import UIkit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- LearnTimer/ViewController.swift
+++ LearnTimer/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer : Timer?
+    var count = 0
+    let settingKey = "timer_value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let settings = UserDefaults.standard
+        settings.register(defaults: [settingKey:10])
     }
 
 
+    @IBOutlet weak var countDownLabel: UILabel!
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonAction(_ sender: Any) {
+    }
+    
 }
 
