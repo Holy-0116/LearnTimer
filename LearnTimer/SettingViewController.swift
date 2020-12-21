aed47201ea99b31935c1187ad00a8c0ec85bdc4e
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-12-21 16:24:24 +0900

    settingViewController

--- LearnTimer/SettingViewController.swift
+++ LearnTimer/SettingViewController.swift
@@ -5,14 +5,25 @@
 //  Created by 堀口達也 on 2020/12/21.
 //
 
-import UIkit
+import UIKit
 
 class SettingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
